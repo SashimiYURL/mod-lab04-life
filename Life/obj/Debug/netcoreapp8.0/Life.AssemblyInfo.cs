fb1e19e10585146c8c2e59759efc737bfb851c02
Sasha <kkkkkkp_16@mail.ru>
2025-04-27 02:22:51 +0300

    fix

--- Life/obj/Debug/netcoreapp8.0/Life.AssemblyInfo.cs
+++ Life/obj/Debug/netcoreapp8.0/Life.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Life")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bedf48c35ef3744417b3b5f8f192d570637650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cebcb27d32a909f0fafb5e53723bdc88ed0aa2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Life")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Life")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
